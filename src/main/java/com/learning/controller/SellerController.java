28c75f56505bf40bcc6dbec116c53d000818fcc4
Berkant <berkantgurcan@hotmail.com>
2022-07-25 14:10:36 +0300

    Cross Origin

--- src/main/java/com/learning/controller/SellerController.java
+++ src/main/java/com/learning/controller/SellerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins="http://localhost:3000")
 public class SellerController {
     @Autowired
     private SellersService sellersService;
